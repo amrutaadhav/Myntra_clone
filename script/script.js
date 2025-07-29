3ed4e459319a502a2f583815b3534e10e3a60ef8
amrutaadhav <amrutaadhav0727@gmail.com>
2025-07-30 02:00:12 +0530

    js for each item added

---
+++ script/script.js
@@ -0,0 +1,25 @@
+let itemContainerElement = document.querySelector(".items-container")
+
+
+let innerHTML = " "
+DataTransferItemList.forEach(item =>{
+ innerHTML += `<div class="item-container">
+        <img class="item-image" src ="${item.image}" alt="img">
+        <div class = "rating">${item.rating.start} ⭐${item.rating.end}  </div>
+        <div class="company">${item.company}</div>
+        <div class="item-name">${item.item_name}</div>
+        <div class="price">
+        <span class="current-price">${item.current_price}</span>
+        <span class="original-price">${item.original_price}</span>
+        <span class="discount">${item.discount}</span>
+        </div>
+        <button class="btn-add-bag">Add to Bag</button>
+        </div>  `
+
+
+})
+
+
+
+
+itemContainerElement.innerHTML = innerHTML
